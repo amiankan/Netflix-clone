d8c6ea92174ac9bc721f06addd02e59a0b5ee3ab
Ankan Das <amiankandas@gmail.com>
2024-05-13 04:23:06 +0530

    adding all files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Netflix-clone
+I have created a netflix clone using React Js and used Firebase in the database
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const Player = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: ""
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YWQ0OWQxMTI4ZGUyOWFhYWEzMzdiYjQ5ZDMyYWY0NCIsInN1YiI6IjY2NDA4ODIxNjQ1MDA0OGE0YzIwNzE0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nh26V4n-KftipW--HYu4-y92HrjW1KLj5wL8ilp7ZnU'
+    }
+  };
+  
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+
+  },[])
+
+  return (
+    <div className='player'>
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
+      <iframe width='90%' height='90%'
+      src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  )
+} 
+
+export default Player
